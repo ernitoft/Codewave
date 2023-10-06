99cd3b3a24e76a7159ab13cc0c609ff8b1362d3a
Gumalto <abraham.cespedes@alumnos.ucn.cl>
2023-10-06 00:13:34 -0300

    feat:cambiosMenores

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+
+@section('title')
+    Página Principal
+@endsection
+
+
+@section('content')
+    <h1 class="text-white text-3xl text-center font-bold uppercase my-4">Bienvenido a Turjoy</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,18 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+})->name('home');
+Route::get('login', function () {
+    return view('auth.login');
+})->name('login');
+
+Route::post('login', [AuthController::class, 'login'])->name('auth.login');
+Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard', [UserController::class, 'dashboardIndex'])->name('dashboard');
+    Route::get('/add/travel', [TravelController::class, 'indexAddTravels'])->name('travels.index');
+    Route::post('/addtravel', [TravelController::class, 'travelCheck'])->name('travel.check');
+    Route::get('/result/travels', [TravelController::class, 'indexTravels'])->name('travelsAdd.index');
 });
